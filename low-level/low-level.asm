84dcbd702e5088e051ff5fda9800839038f9030e
Adrian Kuca <adriankuca2@gmail.com>
2022-01-07 19:05:25 +0100

    Fix the simd stage (proper calculating of pixel addresses)

--- low-level/low-level.asm
+++ low-level/low-level.asm
@@ -1,14 +1,14 @@
 .data
-	block_size dw 32
 	divisor dw 9
+	block_size dw 32
 .code
 	filter_low proc 
 		; arguments : const BYTE* input_image, BYTE* output_image, const int width, const int height
 		; RCX input_image; RDX output_image; R8 width; R9 height
 		mov r11, 0 ; r11 is x_index
 		mov r12, 0 ; r12 is y_index
-		mov r13, RCX ; r13 is input_image
-		mov r14, RDX ; r14 is output_image
+		mov r13, RCX ; r13 is input_image address
+		mov r14, RDX ; r14 is output_image address
 		
 		cmp r8, 32
 		jl calculate_width_remainder ; Skip simd for width < 32 pixels
@@ -21,9 +21,21 @@
 		mov r11, 0 ; r11 is x_index
 	x_loop:
 		; Load 32 bytes from 3 next rows and sum them into ymm0
-		vmovntdqa ymm1, [r13 + r11*32]
-		vmovntdqa ymm2, [r13+ (r12*r8)+1 + r11*32 ]
-		vmovntdqa ymm3, [r13+ (r12*r8)+2 + r11*32 ]
+		mov rax, r11
+		mul block_size
+		mov r15, rax
+		mov rax, r12
+		mul r8
+		add rax, r15
+		add rax, r13
+		vmovdqu  ymm1, ymmword ptr [rax] ; r13 + r11*32 + r12*r8
+
+		add rax, r8
+		vmovdqu ymm2, ymmword ptr [rax] ; r13 + r11*32 + (r12+1)*r8
+
+		add rax, r8
+		vmovdqu ymm3, ymmword ptr [rax] ; r13 + r11*32 + (r12+2)*r8)
+
 		vpaddb ymm4, ymm1, ymm2
 		vpaddb ymm0, ymm3, ymm4
 		
@@ -48,8 +60,17 @@
 		vpbroadcastw ymm8, rax
 		vpmulhrsw ymm0, ymm0, ymm8
 
-		; Save ymm0 into next row of output image
-		vmovntdq [r14 + (r12+1)*r8 +(r11+1)*32 , ymm0]
+		; Save ymm0 into next row of output image [r14 + (r12+1)*r8 +(r11+1)*32]
+		mov rax, r11
+		inc rax
+		mul block_size
+		mov r15, rax
+		mov rax, r12
+		inc rax
+		mul r8
+		add rax, r15
+		add rax, r14
+		vmovntdq ymmword ptr [rax], ymm0
 
 		; Increment r11 which means go right by 32 pixels
 		inc r11
@@ -58,8 +79,8 @@
 		; Increment r12 which means go down by 1 row
 		inc r12
 		; Check if we are at the end of the image
-		mov r15, r9
-		sub r15, 2
+		mov rax, r9
+		sub rax, 2
 		cmp r12, rax
 		je calculate_width_remainder
 		jmp y_loop
