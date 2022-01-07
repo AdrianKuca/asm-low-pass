9d39fac6e96f4cd863c4424a7bba0c6f1bdde277
Adrian Kuca <adriankuca2@gmail.com>
2022-01-07 16:03:11 +0100

    First part of fixing improper register uses

--- low-level/low-level.asm
+++ low-level/low-level.asm
@@ -58,7 +58,9 @@
 		; Increment r12 which means go down by 1 row
 		inc r12
 		; Check if we are at the end of the image
-		cmp r12, r9-2
+		mov r15, r9
+		sub r15, 2
+		cmp r12, rax
 		je calculate_width_remainder
 		jmp y_loop
 
@@ -67,8 +69,10 @@
 		mov rax, r8
 		div block_size
 		mov r10, rdx
-		mov r10, r8-r10
-		mov ecx, r8
+		mov rax, r8
+		sub rax, r10
+		mov r10, rax
+		mov rcx, r8
 		xor r12, r12
 	y_single_pixel_loop:
 		xor r11, r11
@@ -95,19 +99,21 @@
 		loop x_single_pixel_loop
 
 		; Increment r12 which means go down by 1 row
-		mov r12, r12+1
+		inc r12
 		
 		; Check if we are at the end of the image
-		cmp r12, r9-2
+		mov rax, r9
+		sub rax, 2
+		cmp r12, rax
 		je recalculate_side_pixels
-		mov ecx, r8
+		mov rcx, r8
 		jmp y_single_pixel_loop
 	recalculate_side_pixels:
 		; Recalculate every 1st and 32th pixel of the image
 		xor r12, r12
 	y_single_pixel_loop_recalc:
 		xor r11, r11
-		mov ecx, r8
+		mov rcx, r8
 	x_single_pixel_loop_recalc:
 		; Recalculate every first pixel of 32 bytes
 			; Add neighbouring pixels from the same row
@@ -142,7 +148,7 @@
 			; Divide by 9
 			mov rax, rax / 9
 			; Save result into [RDX+ (r11+1)]
-			mov [r14 + (r12+1)*r8 + (31+32*r11+1), rax]
+			mov [r14 + (r12+1)*r8 + (31+32*r11+1)], rax
 
 		; Increment r11 which means go right by 1 pixel
 		inc r11
@@ -152,7 +158,9 @@
 		inc r12
 
 		; Check if we are at the end of the image
-		cmp r12, r9-2
+		mov rax, r9
+		sub rax, 2
+		cmp r12, rax
 		je clear_edges
 		jmp y_single_pixel_loop_recalc
 	clear_edges:
@@ -161,14 +169,16 @@
 		mov rcx, r8
 	x_loop_clear_first:
 		; clear 0th line
-		mov [r14 + (r11), 0]
+		mov rax, r14
+		add r11
+		mov [rax], 0
 		inc r11
 		loop x_loop_clear_first
 		xor r11, r11
 		mov rcx, r8
 	x_loop_clear_last:
 		; clear last line
-		mov [r14 + (r9-1)*r8 + (r11), 0]
+		mov [r14 + (r9-1)*r8 + (r11)], 0
 		inc r11
 		loop x_loop_clear_last
 		xor r11, r11
@@ -176,8 +186,8 @@
 		
 	y_loop_clear:
 		; clear first and last column
-		mov [r14 + r12*r8, 0]
-		mov [r14 + r12*r8 + r8-1, 0]
+		mov [r14 + r12*r8], 0
+		mov [r14 + r12*r8 + r8-1], 0
 		inc r12
 		loop y_loop_clear
 
